b5f59600f13f4a28cc303212e4ae36224876a7dc
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-28 16:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  return driver.key = value 
+   driver.key = value 
+   return driver
 
 }
