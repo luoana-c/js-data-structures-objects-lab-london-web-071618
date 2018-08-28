e2842bfc081c2970ab9971c83835eeab1fc2b235
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-28 16:47:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 }
 
-function deleteFromDriverByKey(driver, key) {
+function destructivelyDeleteFromDriverByKey()(driver, key) {
   delete driver[key]
   return driver
 }
